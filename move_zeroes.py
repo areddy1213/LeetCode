3f17af423aca6c14ed77b7764bfd3c672155b2b0
Abhishek Reddy Y N <areddy1213@gmail.com>
2023-09-14 21:59:41 -0700

    Create move_zeroes.py
    283. Move Zeroes

---
+++ move_zeroes.py
@@ -0,0 +1,14 @@
+###  The idea is the array between 0 to pos has all the existing elements
+###  appeared till now.
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        i = 0
+        pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[pos], nums[i] = nums[i], nums[pos]
+                pos += 1
+        return nums
